0a0d7be94d9403f1f78c76cf739115935b77bb6c
FernandesFilipe <Filipegoulartfernandes@gmail.com>
2024-10-18 19:34:07 +0100

    tips

--- app/src/main/java/com/example/tips_calculate/MainActivity.kt
+++ app/src/main/java/com/example/tips_calculate/MainActivity.kt
@@ -1,20 +1,81 @@
 package com.example.tips_calculate
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tips_calculate.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+        private lateinit var binding: ActivityMainBinding
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            var percentage: Int = 0
+            binding.rb10.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked){
+                    percentage = 10
+                }
+            }
+
+            binding.rb15.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked){
+                    percentage = 15
+                }
+            }
+
+            binding.rb20.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked){
+                    percentage = 20
+                }
+            }
+
+            binding.btnClean.setOnClickListener {
+                binding.Price.setText("")
+                binding.Numberp.setText("")
+                //binding.tvresult.setText("")
+                binding.rb10.isChecked = false
+                binding.rb15.isChecked = false
+                binding.rb20.isChecked = false
+            }
+
+        binding.btnCalculate.setOnClickListener {
+            val totalaccount = binding.Price.text
+            val totalpeople = binding.Numberp.text
+
+
+            if (totalaccount?.isEmpty() == true || totalpeople?.isEmpty() == true) {
+                Snackbar.make(binding.Price,
+                        "Preencha todos os campos",
+                        Snackbar.LENGTH_LONG ) . show()
+            } else {
+
+                val totalac: Float = totalaccount.toString().toFloat()
+                val totalp : Float = totalpeople.toString().toFloat()
+                val account = totalac / totalp
+                val tips = account * percentage / 100
+                val tot = tips + account
+
+                val intent = Intent (this, SegundaTela::class.java )
+                intent.apply {
+                    putExtra("Result", tot)
+                    putExtra("totalc", totalac)
+                    putExtra("totalp", totalp)
+                    putExtra("tips", percentage)
+                    }
+                startActivity(intent)
+            }
         }
     }
 }
